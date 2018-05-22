4285530d008a89e56b3db36c6d3d0376d69e93b1
Gustav <gupett@kth.se>
2018-05-22 12:44:34 +0200

    Auto stash before merge of "master" and "origin/master"
    for showing array of mask images

--- LoadResults.py
+++ LoadResults.py
@@ -1,7 +1,40 @@
-import cv2
+#import cv2
 import numpy as np
+import os
+import re
 
 def LoadAndShowResult(path, caption=''):
-    img = np.load(path)
-    cv2.imshow(caption, img)
-    cv2.waitkey(0)
+
+    # Might need to be of dtyoe='uint8'
+    img_array = np.zeros((64, 64*4, 3))
+    for (root, dirnames, files) in os.walk(path):
+        for file_name in files:
+            print('File name: ' + str(file_name))
+            fileIndex = int(re.search(r'\d+', file_name).group())
+            print('File index: ' + str(fileIndex))
+            start = ((fileIndex-1) * 64)
+            end = ((fileIndex * 64))
+            print('start: ' + str(start) + ' end: ' + str(end))
+            array = np.load(os.path.join(root, file_name))
+            print('array: ' + str(array))
+            print('Size: ' + str(np.shape(array)))
+            print('Shape: ' + str(np.shape(img_array[:, 0:64, :])))
+            img_array[:, start:end, :] = array
+
+    print('finall image array: ' + str(img_array[:][:][0]))
+    #cv2.imshow(caption, img)
+    #cv2.waitkey(0)
+
+def saveNP():
+    array1 = np.random.rand(64,64,3)
+    np.save('./history/attay1', array1)
+    array2 = np.random.rand(64, 64, 3)
+    np.save('./history/attay2', array2)
+    array3 = np.random.rand(64, 64, 3)
+    np.save('./history/attay3', array3)
+    array4 = np.random.rand(64, 64, 3)
+    np.save('./history/attay4', array4)
+
+if __name__=='__main__':
+    #run = saveNP()
+    run = LoadAndShowResult('./history')
